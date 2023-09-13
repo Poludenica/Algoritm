12c019330e20452e667f8a1bb0ca6046ee5998db
Poludnica <svasti.kaula@gmail.com>
2023-09-13 18:16:42 +0300

    first commit

---
+++ Sem2/main.java
@@ -0,0 +1,43 @@
+// Реализовать алгоритм пирамидальной сортировки (сортировка кучей).
+public class main {
+    public static void sort(int[] array){
+        //Построение кучи(перегруппируем массив)
+        for (int i = array.length / 2 -1; i >= 0; i --)
+            heapify(array, array.length, i);
+        
+        //один за другим извлекаем элементы из кучи
+        for (int i = array.length - 1; i >=0; i--) {
+            int temp = array[0];
+            array[0]= array[i];
+            array[i] = temp;
+            //вызываем процедуру heapify на уменьшенной куче
+            heapify(array, i, 0);
+        }
+
+
+    }
+    private static void heapify(int[] array, int heapSize, int rootIndex) {
+        int largest = rootIndex; //иниц-ем наибольший элемент как корень
+        int leftChild = 2 * rootIndex + 1; //левый = 2 * rootIndex + 1
+        int rightChild = 2 * rootIndex + 2; // правый = 2 * rootIndex + 2
+
+        //Если левый дочерний элемент больше корня
+        if (leftChild < heapSize && array[leftChild] > array[largest])
+            largest = leftChild;
+        
+        //Если правый дочерний элемент больше, чем самый большой элемент на данный момент
+        if (rightChild < heapSize && array[rightChild] > array[largest])
+        largest = rightChild;
+
+        //если самый большой элемент не корень
+        if (largest != rootIndex) {
+            int temp = array[rootIndex];
+            array[rootIndex] = array[largest];
+            array[largest]= temp;
+        
+        //рекурсивно преобразуем в двоичную кучу затронутое поддерево
+        heapify(array, heapSize, largest);
+        }
+    }
+}
+
